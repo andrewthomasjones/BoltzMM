4814a1eeef92c885dd21de335f2506658eca64ea
Andrew Jones <andrewjones@192-168-1-20.tpgi.com.au>
2019-01-05 16:58:09 +1000

    more tests for new functions

--- tests/testthat/test-rfunct.R
+++ tests/testthat/test-rfunct.R
@@ -30,3 +30,29 @@ test_that("Check that fvbmstderr calculates stderr Correctly.",{
   expect_known_output(stderrResult, tmp2, print = TRUE)
 
 })
+
+test_that("Check that marginpfvbm calculates marginal probailities Correctly.",{
+  bvec <- c(0,0.5,0.25)
+  Mmat <- matrix(0.1,3,3) - diag(0.1,3,3)
+  marginResult<-marginpfvbm(bvec, Mmat)
+  tmp2 <- "./marginpfvbm"
+
+  # The first run always succeeds, but warns
+  expect_known_output(marginResult, tmp2, print = TRUE)
+
+})
+
+test_that("Check that fvbmtests calculates scores and p-values correctly.",{
+  set.seed(1)
+  bvec <- c(0,0.5,0.25)
+  Mmat <- matrix(0.1,3,3) - diag(0.1,3,3)
+  num <- 1000
+  data <- rfvbm(num,bvec,Mmat)
+  model <- fitfvbm(data,bvec,Mmat)
+  nullmodel <- list(bvec = c(0,0,0), Mmat = matrix(0,3,3))
+  testResult<-fvbmtests(data,model,nullmodel)
+  tmp2 <- "./fvbmtests"
+  # The first run always succeeds, but warns
+  expect_known_output(testResult, tmp2, print = TRUE)
+
+})
